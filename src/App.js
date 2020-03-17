a21176da4ebfda24987ac9a374f460c008360b05
Ketan <ketantoo@gmail.com>
2020-03-17 14:34:18 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: list
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.list.map(item => (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span> {item.num_comments}</span>
+            <span> {item.points}</span>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
