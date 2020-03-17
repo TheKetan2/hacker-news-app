019775181dec413f87665eeda9cb00a4d057d5ef
Ketan <ketantoo@gmail.com>
2020-03-17 15:34:38 +0530

    Split up component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+
+import Search from "./Search";
+import Table from "./Table";
 
 const list = [
   {
@@ -20,12 +22,6 @@ const list = [
     objectID: 1
   }
 ];
-
-function isSearched(searchTerm) {
-  return function(item) {
-    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
-  };
-}
 
 class App extends React.Component {
   constructor(props) {
@@ -52,27 +48,8 @@ class App extends React.Component {
     const { searchTerm, list } = this.state;
     return (
       <div className="App">
-        <form>
-          <input type="text" onChange={this.onSearchChange} />
-        </form>
-        {list.filter(isSearched(searchTerm)).map(item => {
-          const onHandleDismiss = () => this.onDismiss(item.objectID);
-          return (
-            <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span> {item.num_comments}</span>
-              <span> {item.points}</span>
-              <span>
-                <button onClick={onHandleDismiss} type="button">
-                  Dismiss
-                </button>
-              </span>
-            </div>
-          );
-        })}
+        <Search value={searchTerm} onChange={this.onSearchChange} />
+        <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
       </div>
     );
   }
