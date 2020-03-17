5ace6b6c954f2f9913edc3a45f118fff56970584
Ketan <ketantoo@gmail.com>
2020-03-17 15:59:25 +0530

    converting class based component to functional component

--- src/Search.js
+++ src/Search.js
@@ -1,15 +1,13 @@
 import React from "react";
 
-class Search extends React.Component {
-  render() {
-    const { value, onChange, children } = this.props;
-    return (
-      <form>
-        {children}
-        <input type="text" value={value} onChange={onChange} />
-      </form>
-    );
-  }
-}
+const Search = props => {
+  const { value, onChange, children } = props;
+  return (
+    <form>
+      {children}
+      <input type="text" value={value} onChange={onChange} />
+    </form>
+  );
+};
 
 export default Search;
